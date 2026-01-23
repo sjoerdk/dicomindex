4e81358baa700f2567f0567a286b464be17282de
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2026-01-23 17:07:55 +0100

    Removes test that was dependent on unstable ordering

--- tests/test_iterators.py
+++ tests/test_iterators.py
@@ -15,8 +15,6 @@ def test_patient_folder_iterator(example_dicom_folder):
     path_iter = EagerIterator(folder.folders())
     dirs = list(path_iter)
     assert len(dirs) == 13
-    files = [x for x in dirs[9].glob("*/*") if x.is_file()]
-    assert len(files) == 2
 
 
 def test_eager_iterator():
